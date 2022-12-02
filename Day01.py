cf0cbf317fb6e334d5c0890ffb75d69f545d874d
80utezec <56273079+80utezec@users.noreply.github.com>
2022-12-02 12:14:51 -0600

    Day01

---
+++ Day01.py
@@ -0,0 +1,25 @@
+from Elf import Elf
+
+if __name__ == '__main__':
+  elves = []
+
+  with open("C:/Users/80utezec/dev/python/AdventOfCode2022/inputs/inputDay01.txt") as _file:
+
+    calories = []
+    elfCount = 1
+    for line in _file:
+      if len(line.strip()) == 0:
+        elves.append(Elf(elfCount, calories, sum(calories)))
+
+        calories = []
+        elfCount += 1
+      else:
+        calories.append(int(line))
+
+elves.sort(key=lambda x: x.totalCalories)
+print(f"{elves[-1]}")
+
+best3 = 0
+for elf in elves[-3:]:
+  best3 += elf.totalCalories
+print(f"Best 3: {best3}")
