bc3014f983225aa8dcbe089e06cfb7c87b18f1f0
Zechariah Utecht <zechariah.utecht@menasha.com>
2022-12-02 15:38:06 -0600

    might not be the right way to call methods within a class

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,7 +2,6 @@ shapesValues = {'rock': 1, 'paper': 2, 'scissors': 3}
 
 
 class RockPaperScissors():
-
   def pointTotal(computerAction, playerAction):
     shapeValue = shapesValues[playerAction]
 
@@ -23,3 +22,21 @@ class RockPaperScissors():
         return 6 + shapeValue
       else:
         return 0 + shapeValue
+
+  def winLoseDraw(computerAction, playerDecision):
+    if playerDecision == "draw":
+      return RockPaperScissors.pointTotal(computerAction, computerAction)
+    elif playerDecision == "win":
+      if computerAction == "rock":
+        return RockPaperScissors.pointTotal(computerAction, "paper")
+      elif computerAction == "paper":
+        return RockPaperScissors.pointTotal(computerAction, "scissors")
+      else:
+        return RockPaperScissors.pointTotal(computerAction, "rock")
+    else:
+      if computerAction == "rock":
+        return RockPaperScissors.pointTotal(computerAction, "scissors")
+      elif computerAction == "paper":
+        return RockPaperScissors.pointTotal(computerAction, "rock")
+      else:
+        return RockPaperScissors.pointTotal(computerAction, "paper")
